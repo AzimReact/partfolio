436ab9a78c2d962234a843e24a0abf139afdc2d7
Azim <azimreact@gmail.com>
2023-12-06 12:54:50 +0600

    markup desktop about and gallery pages

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded",function() {
   const menuIcon = document.querySelector(".menu-icon");
   const modal = document.getElementById("myModal");
   const closeModal = document.getElementById("closeModal");
+  const slider = document.querySelector(".swiper-container");
 
   menuIcon.addEventListener("click",function() {
-    // Открываем модальное окно
     modal.style.display = "block";
+    slider.style.zIndex = '-1';
   });
 
   closeModal.addEventListener("click",function() {
-    // Закрываем модальное окно
     modal.style.display = "none";
+    slider.style.zIndex = '1';
   });
 
-  // Закрываем модальное окно при клике вне его области
   window.addEventListener("click",function(event) {
     if (event.target === modal) {
       modal.style.display = "none";
+      slider.style.zIndex = '1';
     }
   });
 });
